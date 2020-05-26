40f3bf8a24796aa7799ce6f279203c3ade0717c3
yurimelosal@outlook.com <yurimelosal@outlook.com>
2020-05-26 17:04:14 -0300

    Alteração Poo

---
+++ ProjetoAtleta35/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace ProjetoAtleta35
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtNascimento = new System.Windows.Forms.DateTimePicker();
+            this.lblIdade = new System.Windows.Forms.Label();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.lblData = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // dtNascimento
+            // 
+            this.dtNascimento.Location = new System.Drawing.Point(175, 95);
+            this.dtNascimento.Name = "dtNascimento";
+            this.dtNascimento.Size = new System.Drawing.Size(217, 20);
+            this.dtNascimento.TabIndex = 19;
+            // 
+            // lblIdade
+            // 
+            this.lblIdade.AutoSize = true;
+            this.lblIdade.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.lblIdade.Location = new System.Drawing.Point(180, 348);
+            this.lblIdade.Name = "lblIdade";
+            this.lblIdade.Size = new System.Drawing.Size(92, 29);
+            this.lblIdade.TabIndex = 18;
+            this.lblIdade.Text = "0 Anos";
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(206, 311);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(40, 13);
+            this.lblInfo.TabIndex = 17;
+            this.lblInfo.Text = "Idade: ";
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Location = new System.Drawing.Point(58, 141);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(40, 13);
+            this.lblAltura.TabIndex = 16;
+            this.lblAltura.Text = "Altura: ";
+            // 
+            // lblData
+            // 
+            this.lblData.AutoSize = true;
+            this.lblData.Location = new System.Drawing.Point(58, 100);
+            this.lblData.Name = "lblData";
+            this.lblData.Size = new System.Drawing.Size(110, 13);
+            this.lblData.TabIndex = 15;
+            this.lblData.Text = "Data de Nascimento: ";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(58, 59);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(41, 13);
+            this.lblNome.TabIndex = 14;
+            this.lblNome.Text = "Nome: ";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(61, 248);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(331, 51);
+            this.btnCalcular.TabIndex = 13;
+            this.btnCalcular.Text = "Calcular idade";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(61, 178);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(331, 53);
+            this.btnEnviar.TabIndex = 12;
+            this.btnEnviar.Text = "Enviar dados";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(175, 138);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(217, 20);
+            this.txtAltura.TabIndex = 11;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(175, 52);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(217, 20);
+            this.txtNome.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 440);
+            this.Controls.Add(this.dtNascimento);
+            this.Controls.Add(this.lblIdade);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.lblData);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.txtNome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtNascimento;
+        private System.Windows.Forms.Label lblIdade;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.Label lblData;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.TextBox txtNome;
+    }
+}
+
